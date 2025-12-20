d8f2791ffc535201930b32685ae242acee72a094
NeyBg23 <mathicuenta23@gmail.com>
2025-12-20 14:06:11 -0500

    admin con algunas rutas no todas

--- app-service/api/routes/cuentaAdminRoutes.js
+++ app-service/api/routes/cuentaAdminRoutes.js
@@ -2,11 +2,16 @@
 import { Router } from 'express';
 import { verifyToken, requireAdmin } from '../middleware/jwtValidate.js';
 import { listarCuentasPorSorteos, entregarCuenta } from '../controllers/cuentaController.js';
+import { requirePermission } from '../middleware/requirePermission.js';
+
 
 const router = Router();
 
 // Por ahora solo admin. En el Paso 3 añadimos requirePermission('cuentas:gestionar')
 router.use(verifyToken, requireAdmin);
+
+// Añadimos middleware de permisos
+router.use(requirePermission('cuentas:gestionar'));
 
 // GET acordeón
 router.get('/sorteos', listarCuentasPorSorteos);
